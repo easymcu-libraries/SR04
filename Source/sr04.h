2397bac7280750850c777b73da55ba751789ae78
Morteza Zandi <mzandi@users.noreply.github.com>
2016-06-16 09:46:52 +0430

    Create sr04.h

---
+++ Source/sr04.h
@@ -0,0 +1,39 @@
+/********************************************************************
+  This is a library for Ultrasonic SR04.
+
+  ------> http://www.easymcu.ir
+  
+  Check out the links above for our tutorials and wiring diagrams
+  
+  Written by Morteza Zandi for EasyMCU.
+  BSD license, All text above must be included in any redistribution
+ ********************************************************************/
+ 
+	#ifndef __SR04_H__
+	#define __SR04_H__ 
+	
+	#include "fio.h"
+	#include "timer.h"
+	#include "delay.h"
+	
+	extern FIO io;
+	extern TIMER timer;
+	extern DELAY delay;
+	
+	class SR04
+	{
+		public:			
+		SR04(PORT_PIN_ARRAY trig_pin = P39, PORT_PIN_ARRAY echo_pin = P40);	
+		void setTrig(PORT_PIN_ARRAY Pnum);
+		void setEcho(PORT_PIN_ARRAY Pnum);
+		float distance(void);
+		
+		private:
+			void trig(void);
+			uint8_t echo(void);	
+			PORT_PIN_ARRAY Trig;
+			PORT_PIN_ARRAY Echo;
+	};
+	
+	#endif
+	
